dcd097e498260aa09dc1bc69b3112e9e6348b2d4
mserebryaakov <maks.serebriakov2019@gmail.com>
2022-02-14 20:15:57 +0300

    APIcomponentContainer

--- src/components/Users/UsersPage.jsx
+++ src/components/Users/UsersPage.jsx
@@ -1,52 +1,21 @@
 import s from './UsersPage.module.css'
-import UserProfile from './UserProfile/UserProfile'
 import React from 'react'
-import * as axios from 'axios'
 
-class UsersPage extends React.Component {
+let UsersPage = (props) => {
+    let pageCount = Math.ceil(props.totalUserCount / props.pageSize);
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setUsersTotalCount(response.data.totalCount);
-        });
-    }
-
-    createUsersElements = () => {
-        return (
-            this.props.allUsersProfiles
-                .map(element => <UserProfile state={element} follow={this.props.follow} key={element.id} />)
-        )
-    }
-
-    updateCurrentPage = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        });
-    }
-
-    render() {
-
-        let pageCount = Math.ceil(this.props.totalUserCount / this.props.pageSize);
-
-        return (
-            <div className={s.content}>
-                <div>
-                    <span onClick={() => { this.updateCurrentPage(this.props.currentPage - 1) }}>Назад </span>
-                    <span>{this.props.currentPage}</span>
-                    <span>...</span>
-                    <span>{pageCount - 1}</span>
-                    <span onClick={() => { this.updateCurrentPage(this.props.currentPage + 1) }}> Вперед</span>
-                </div>
-                {/* { pages.map ( element => {
-                    return <span className={this.props.currentPage === element && s.selectedPage}
-                    onClick={ () => { this.updateCurrentPage(element)}}>{element}</span>
-                })} */}
-                {this.createUsersElements()}
+    return (
+        <div className={s.content}>
+            <div>
+                <span onClick={() => { props.updateCurrentPage(props.currentPage - 1) }}>Назад </span>
+                <span>{props.currentPage}</span>
+                <span>...</span>
+                <span>{pageCount - 1}</span>
+                <span onClick={() => { props.updateCurrentPage(props.currentPage + 1) }}> Вперед</span>
             </div>
-        );
-    }
+            {props.createUsersElements()}
+        </div>
+    );
 }
 
 export default UsersPage;
--- src/components/Users/UsersPageContainer.jsx
+++ src/components/Users/UsersPageContainer.jsx
@@ -1,6 +1,43 @@
 import { followActionCreater, setUsersActionCreater, setCurrentPageActionCreater, setUsersTotalCountActionCreater } from '../../redux/users-reducer'
-import UsersPage from './UsersPage'
+import UsersPage from './UsersPage';
 import { connect } from 'react-redux';
+import React from 'react';
+import UserProfile from './UserProfile/UserProfile';
+import * as axios from 'axios';
+
+class UsersPageAPIContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setUsersTotalCount(response.data.totalCount);
+        });
+    }
+
+    createUsersElements = () => {
+        return (
+            this.props.allUsersProfiles
+                .map(element => <UserProfile state={element} follow={this.props.follow} key={element.id} />)
+        )
+    }
+
+    updateCurrentPage = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        });
+    }
+
+    render() {
+        return (
+            <UsersPage totalUserCount={this.props.totalUserCount}
+            pageSize={this.props.pageSize} 
+            updateCurrentPage={this.updateCurrentPage}
+            currentPage={this.props.currentPage}
+            createUsersElements={this.createUsersElements}/>
+        )
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -28,7 +65,7 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersPageContainer = connect(mapStateToProps, mapDispatchToProps)(UsersPage);
+const UsersPageContainer = connect(mapStateToProps, mapDispatchToProps)(UsersPageAPIContainer);
 
 
 export default UsersPageContainer;
